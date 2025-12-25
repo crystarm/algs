3f20ba981deb266c694f0970834df7dde003243e
Georgij Tsarin <68424751+crystarm@users.noreply.github.com>
2025-12-25 16:31:31 +0300

    add 0293 coderun (just SQL)

---
+++ coderun/0293.sql
@@ -0,0 +1,16 @@
+WITH t AS (
+  SELECT
+    user_id,
+    time,
+    LAG(time) OVER (PARTITION BY user_id ORDER BY time) AS prev_time
+  FROM logs
+)
+SELECT
+  user_id,
+  SUM(CASE
+        WHEN prev_time IS NULL OR time - prev_time > 30 THEN 1
+        ELSE 0
+      END) AS cnt
+FROM t
+GROUP BY user_id
+ORDER BY user_id;
